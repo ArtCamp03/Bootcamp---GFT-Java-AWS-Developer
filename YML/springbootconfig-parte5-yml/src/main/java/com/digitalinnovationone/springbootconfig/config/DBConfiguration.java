487a05776118b19a6f93d1775e47fe61117ecf2c
Artur R Campos <arturrc10@outlook.com>
2021-10-18 14:40:36 -0300

    att springProfiles

--- YML/springbootconfig-parte5-yml/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
+++ YML/springbootconfig-parte5-yml/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
@@ -1,39 +1,38 @@
-package com.DigitalInovationOne.springbootconfig.config;
+package com.digitalinnovationone.springbootconfig.config;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import javax.annotation.processing.Generated;
-
 @Configuration
 @ConfigurationProperties("spring.datasource")
 @Getter
 @Setter
-
 public class DBConfiguration {
+
     private String driverClassName;
     private String url;
-    private String userName;
+    private String username;
     private String password;
 
-        @Profile("dev")
-        @Bean
-    public String testeDataBaseConnection(){
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
         System.out.println("DB connection for DEV - H2");
         System.out.println(driverClassName);
         System.out.println(url);
-        return "DB connection to H2_TEST - test instance "
+        return "DB Connection to MYSQL_TEST - Test instance";
     }
 
-        @Profile("prod")
-        @Bean
-    public String productionDataBaseConnection(){
-        System.out.println("DB connection for PRODUCTION - MYSQL");
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+        System.out.println("DB connection for MYSQL Database: production");
         System.out.println(driverClassName);
         System.out.println(url);
-        return "DB connection to MYSQL_PROD - Production instance "
+        return "DB Connection to MySql production - Test instance";
     }
 }
