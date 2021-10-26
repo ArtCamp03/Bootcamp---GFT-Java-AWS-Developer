4f5c0ba14ed3bf68949298337e7a37dd54642e0b
Artur R Campos <arturrc10@outlook.com>
2021-10-26 13:19:33 -0300

    att

--- JAVA-avaçado/Interface/src/Predicados/Predicates.java
+++ JAVA-avaçado/Interface/src/Predicados/Predicates.java
@@ -1,7 +1,10 @@
 package Predicados;
 
+import java.util.function.Predicate;
+
 public class Predicates {
     public static void main(String[] args) {
-        
+        Predicate<String> EstaVazio = valor -> valor.isEmpty();
+        System.out.println(EstaVazio.test("ola"));
     }
 }
